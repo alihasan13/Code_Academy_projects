142120683b16a00faabbd5f449a94f09de2b8ded
Ali Hasan <ali.hasan@northsouth.edu>
2018-09-20 04:54:16 +0600

    code changed

--- pig_latin_translator.py
+++ pig_latin_translator.py
@@ -1,8 +1,10 @@
 
+print ('Wellcome')
 word= input("Enter your word: ")
-if word=="":
-    print ("You have entered nothing.")
-else:
+if word != "" and word.isalpha():  #isalpha will prevent words containing digit/sympbols
+    print ("you have entered: "+word)
     translate_pig= word[1:]+word[0]+"ay"
     print (translate_pig.lower())
 
+else:
+    print ("you have entered nothing or the word contains numbers/symbles")
